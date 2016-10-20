3d16e7a93cd7deea7ab5548338d1838762da5e91
rienafairefr <rienafairefr@gmail.com>
2016-10-20 10:09:22 +0200

    [ci-cron]
    [ci-cron]
    
    test [ci-cron]
    
    def test_sync [ci-cron]
    
    [ci-cron]
    
    no message
    
    [ci-cron]
    
    [ci-cron]test

--- test_sync/test_sync.py
+++ test_sync/test_sync.py
@@ -1,8 +1,17 @@
+import unittest
+
 from pynYNAB.Client import clientfromargs
 from pynYNAB.scripts.config import parser
 
-# used to ping the nYNAB API to check that the sync works
 
-args = parser.parse_known_args()[0]
-client = clientfromargs(args)
-client.sync()
+# used to ping the nYNAB API to check that the sync works
+class TestSync(unittest.TestCase):
+    @staticmethod
+    def test_sync():
+        print('test_sync')
+        args = parser.parse_known_args()[0]
+        client = clientfromargs(args)
+        client.sync()
+
+if __name__ == '__main__':
+    TestSync.test_sync()
