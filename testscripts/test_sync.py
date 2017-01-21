7a04ca6ae970b222a494c0cfc357af4d6b3eb1d1
rienafairefr <rienafairefr@gmail.com>
2017-01-21 14:19:52 +0100

    dotenv env files for development and testing

--- testscripts/test_sync.py
+++ testscripts/test_sync.py
@@ -1,7 +1,9 @@
 from pynYNAB.Client import clientfromargs
 from pynYNAB.scripts.config import parser
+from dotenv import load_dotenv,find_dotenv
+load_dotenv(find_dotenv())
 
-print('test_live')
+print('test_sync')
 args = parser.parse_known_args()[0]
 client = clientfromargs(args)
 client.sync()
