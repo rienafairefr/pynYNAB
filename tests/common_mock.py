5ea5c747573a1c23fb309c596d179bf00565512d
rienafairefr <rienafairefr@gmail.com>
2018-01-26 17:27:24 +0100

    rewrite common mock for testing

--- tests/common_mock.py
+++ tests/common_mock.py
@@ -23,38 +23,43 @@ class MockConnection(object):
         pass
 
     user_id = '1'
-factory = nYnabClientFactory()
+
+
+def setupEmptyClient():
+    factory = nYnabClientFactory()
+    client = factory.create_client(budget_name='TestBudget',
+                                   connection=MockConnection(),
+                                   sync=False)
+
+    session = client.session
+
+    budget_version = BudgetVersion(version_name='TestBudget')
+    master_category = MasterCategory(name='master')
+    subcategory = SubCategory(name='Immediate Income',
+                              internal_name='Category/__ImmediateIncome__',
+                              entities_master_category=master_category)
+    payee = Payee(name='Starting Balance Payee', internal_name='StartingBalancePayee')
+    session.add(master_category)
+    session.add(subcategory)
+    session.add(payee)
+
+    client.catalog.ce_budget_versions.append(budget_version)
+    client.budget.be_master_categories.append(master_category)
+    client.budget.be_subcategories.append(subcategory)
+    client.budget.be_payees.append(payee)
+    session.commit()
+    client.budget.clear_changed_entities()
+    client.catalog.clear_changed_entities()
+
+    client.budgetClient.device_knowledge_of_server = 0
+    client.catalogClient.device_knowledge_of_server = 0
+
+    client.budgetClient.current_device_knowledge = 0
+    client.catalogClient.current_device_knowledge = 0
+
+    return client
+
 
 class TestCommonMock(unittest.TestCase):
     def setUp(self):
-        self.client = factory.create_client(budget_name='TestBudget',
-                                            connection=MockConnection(),
-                                            sync=False)
-
-        session = self.client.session
-
-        budget_version = BudgetVersion(version_name='TestBudget')
-        master_category = MasterCategory(name='master')
-        subcategory = SubCategory(name='Immediate Income',
-                                  internal_name='Category/__ImmediateIncome__',
-                                  entities_master_category=master_category)
-        payee = Payee(name='Starting Balance Payee',internal_name='StartingBalancePayee')
-        session.add(master_category)
-        session.add(subcategory)
-        session.add(payee)
-
-        self.client.catalog.ce_budget_versions.append(budget_version)
-        self.client.budget.be_master_categories.append(master_category)
-        self.client.budget.be_subcategories.append(subcategory)
-        self.client.budget.be_payees.append(payee)
-        session.commit()
-        self.client.budget.clear_changed_entities()
-        self.client.catalog.clear_changed_entities()
-
-        self.client.budgetClient.device_knowledge_of_server = 0
-        self.client.catalogClient.device_knowledge_of_server = 0
-
-        self.client.budgetClient.current_device_knowledge = 0
-        self.client.catalogClient.current_device_knowledge = 0
-
-        pass
+        self.client = setupEmptyClient()
