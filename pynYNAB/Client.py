50885eb4e40017d931413795095bb04f9a05b090
Benjamin Dabelow <b-git@dabelow.eu>
2018-01-27 23:53:49 +0100

    Fix linter errors.

--- pynYNAB/Client.py
+++ pynYNAB/Client.py
@@ -8,9 +8,11 @@ LOG = logging.getLogger(__name__)
 class nYnabClient(object):
     def __new__(cls, *args, **kwargs):
         connection = kwargs.pop('nynabconnection', None)
-        email = connection.email if hasattr(connection, 'email') else kwargs.pop('email', '')
-        password = connection.password if hasattr(connection, 'password') else kwargs.pop('password', '')
+        email = connection.email if hasattr(connection, 'email')\
+            else kwargs.pop('email', '')
+        password = connection.password if hasattr(connection, 'password')\
+            else kwargs.pop('password', '')
         budget_name = kwargs.pop('budgetname', None)
-        
+
         factory = nYnabClientFactory()
         return factory.create_client(email, password, budget_name, connection)
