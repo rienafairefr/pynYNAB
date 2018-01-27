70ada958827a58738b15013381ba9048f591c008
Benjamin Dabelow <b-git@dabelow.eu>
2018-01-27 23:34:38 +0100

    Adapt for new create_client interface.

--- pynYNAB/Client.py
+++ pynYNAB/Client.py
@@ -1,7 +1,6 @@
 import logging
 
 from pynYNAB.ClientFactory import nYnabClientFactory
-from pynYNAB.schema import nYnabClient_
 
 LOG = logging.getLogger(__name__)
 
@@ -9,13 +8,9 @@ LOG = logging.getLogger(__name__)
 class nYnabClient(object):
     def __new__(cls, *args, **kwargs):
         connection = kwargs.pop('nynabconnection', None)
-
-        class Args(object):
-            budget_name = kwargs.pop('budgetname', None)
-            email = connection.email if hasattr(connection, 'email') else kwargs.pop('email', '')
-            password = connection.password if hasattr(connection, 'password') else kwargs.pop('password', '')
-            nynabconnection = connection
-
-        passed = Args()
+        email = connection.email if hasattr(connection, 'email') else kwargs.pop('email', '')
+        password = connection.password if hasattr(connection, 'password') else kwargs.pop('password', '')
+        budget_name = kwargs.pop('budgetname', None)
+        
         factory = nYnabClientFactory()
-        return factory.create_client(passed)
+        return factory.create_client(email, password, budget_name, connection)
