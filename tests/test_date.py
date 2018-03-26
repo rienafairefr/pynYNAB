2fd3bfbfd8ff0629b130496e2f075e974ff83b2b
rienafairefr <rienafairefr@gmail.com>
2018-03-26 23:23:49 +0200

    more tests migrated

--- tests/test_date.py
+++ tests/test_date.py
@@ -1,5 +1,3 @@
-import unittest
-import uuid
 import re
 from datetime import datetime
 
@@ -11,14 +9,9 @@ date0 = datetime.strptime(strdate0, '%Y-%m-%d').date()
 datestrings = [strdate0+'T00:00:00',strdate0,'random+'+strdate0]
 
 
-class TestDatesCases(unittest.TestCase):
-    def do_test(self, x, test_name):
-        self.assertEqual(date_from_api(None, x), date0, msg=test_name)
+def test_dates():
+    # Test some other functionality here
 
-    def test_dates(self):
-        pass
-        # Test some other functionality here
-
-        for s in datestrings:
-            test_name = 'test_datefromapi_%s' % re.sub('[^a-zA-Z0-9]+', '', s)
-            self.do_test(s, test_name)
+    for s in datestrings:
+        test_name = 'test_datefromapi_%s' % re.sub('[^a-zA-Z0-9]+', '', s)
+        assert date_from_api(None, s) == date0
--- tests/test_entities.py
+++ tests/test_entities.py
@@ -2,8 +2,9 @@ from __future__ import absolute_import
 
 import datetime
 import json
-import unittest
+from collections import namedtuple
 
+import pytest
 from sqlalchemy import Column
 from sqlalchemy import Date
 from sqlalchemy import Enum
@@ -24,191 +25,132 @@ class MyEntity(Base, Entity):
     greatfield = Column(Integer, default=2)
 
 
-class CommonTest(unittest.TestCase):
-    def setUp(self):
-        engine = create_engine('sqlite:///:memory:')
+@pytest.fixture
+def session():
+    engine = create_engine('sqlite:///:memory:')
 
-        Base.metadata.create_all(engine)
-        self.Session = sessionmaker(bind=engine)
-        self.session = self.Session()
-
-factory = nYnabClientFactory()
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    return Session()
 
 
-class TestGetChangedEntities(CommonTest):
-    def setUp(self):
-        super(TestGetChangedEntities, self).setUp()
-        self.obj = Budget()
-        self.account = Account()
-        self.obj.be_accounts = [self.account]
-        self.obj.clear_changed_entities()
-        self.account2 = Account(id=self.account)
-
-        self.client = factory.create_client(budget_name='budgetname', connection = MockConnection(),sync=False)
-
-    def testgetChangedEntities_add(self):
-        added_account = Account()
-        self.obj.be_accounts.append(added_account)
-        changed_entities = self.obj.get_changed_entities()
-        self.assertEqual(changed_entities, {'be_accounts': [added_account]})
-
-    def testgetChangedEntities_addtransactionsubtransaction(self):
-        added_transaction = Transaction()
-        subtransaction1 = Subtransaction(entities_transaction=added_transaction)
-        subtransaction2 = Subtransaction(entities_transaction=added_transaction)
-
-        self.client.budget.be_transactions.append(added_transaction)
-        self.client.budget.be_subtransactions.append(subtransaction1)
-        self.client.budget.be_subtransactions.append(subtransaction2)
-
-        self.client.session.commit()
-
-        changed_entities = self.client.budget.get_changed_entities()
-        self.assertIsInstance(changed_entities,dict)
-        self.assertEqual(1, len(changed_entities.keys()))
-        self.assertEqual('be_transaction_groups',list(changed_entities.keys())[0])
-        transaction_groups = changed_entities['be_transaction_groups']
-
-        self.assertEqual(1, len(transaction_groups))
-        self.assertEqual(added_transaction, transaction_groups[0]['be_transaction'])
-
-        self.assertIsNotNone(transaction_groups[0]['be_subtransactions'])
-        try:
-            self.assertItemsEqual([subtransaction1,subtransaction2], set(transaction_groups[0]['be_subtransactions']))
-        except AttributeError:
-            self.assertCountEqual([subtransaction1,subtransaction2], set(transaction_groups[0]['be_subtransactions']))
+@pytest.fixture
+def account():
+    return Account()
 
 
-    def testgetChangedEntities_delete(self):
-        self.obj.be_accounts.remove(self.account)
-        changed_entities = self.obj.get_changed_entities()
-        deleted = self.account.copy()
-        deleted.is_tombstone = True
-        self.assertEqual(changed_entities, {'be_accounts': [deleted]})
-
-    def testgetChangedEntities_modify(self):
-        self.account.account_name = 'BLA'
-        changed_entities = self.obj.get_changed_entities()
-        self.assertEqual(changed_entities, {'be_accounts': [self.account]})
-
-    def test_arraytype(self):
-        user = User()
-        user.feature_flags = ['featureA','feature1']
-        self.session.add(user)
-        self.session.commit()
-
-        fetched_user = self.session.query(User).first()
-        self.assertEqual(user,fetched_user)
+@pytest.fixture
+def client(account):
+    client = nYnabClientFactory().create_client(budget_name='budgetname',
+                                                connection=MockConnection(),
+                                                sync=False)
+    client.budget.be_accounts = [account]
+    client.session.commit()
+    client.budget.clear_changed_entities()
+    return client
 
 
-class TestUpdateChangedEntities(CommonTest):
-    def setUp(self):
-        self.account = Account()
-
-        self.client = factory.create_client(budget_name='budgetname',
-                                            connection = MockConnection(),
-                                            sync=False)
-        self.client.budget.be_accounts = [self.account]
-        self.account2 = self.account.copy()
-        self.client.session.commit()
-
-    def testupdateChangedEntities_add(self):
-        new_account = Account()
-        changed_entities = dict(
-            be_accounts=[new_account]
-        )
-        self.client.budgetClient.update_from_changed_entities(changed_entities)
-        self.assertEqual(len(self.client.budget.be_accounts), 2)
-        self.assertIn(new_account, self.client.budget.be_accounts)
-
-    def testupdateChangedEntities_delete(self):
-        self.account2.is_tombstone = True
-        changed_entities = dict(
-            be_accounts=[self.account2]
-        )
-        self.client.budgetClient.update_from_changed_entities(changed_entities)
-        self.assertEqual(len(self.client.budget.be_accounts), 0)
-
-    def testupdateChangedEntities_modify(self):
-        self.account2 = self.account.copy()
-        self.account2.note = 'note'
-        changed_entities = dict(
-            be_accounts=[self.account2]
-        )
-
-        self.client.budgetClient.update_from_changed_entities(changed_entities)
-        self.assertEqual(len(self.client.budget.be_accounts), 1)
-        acc = self.client.budget.be_accounts[0]
-        self.assertEqual(acc, self.account2)
+@pytest.fixture
+def obj_w_account(account):
+    ObjWAccount = namedtuple('ObjWAccount', ['obj', 'account'])
+    budget = Budget()
+    budget.be_accounts = [account]
+    budget.clear_changed_entities()
+    return ObjWAccount(budget, account)
 
 
-class OtherTests(CommonTest):
-    def testEntityjson(self):
-        obj = MyEntity()
-        self.session.add(obj)
-        self.session.commit()
-
-        jsonroundtrip = json.loads(json.dumps(obj, cls=ComplexEncoder))
-        self.assertEqual({'id': str(obj.id), 'greatfield': 2, 'is_tombstone': False}, jsonroundtrip)
-
-    def testequality(self):
-        tr1 = Transaction(id='t')
-        tr2 = Transaction(id='t')
-        self.assertTrue(tr1 == tr2)
-
-        tr1 = Transaction()
-        tr2 = Transaction()
-        self.assertFalse(tr1 == tr2)
-
-    def testrepr(self):
-        tr1 = Transaction(id='t')
-        self.assertEqual(tr1.__repr__(),tr1.__str__())
-
-    def testappend(self):
-        obj = Budget()
-        account = Account()
-        obj.be_accounts.append(account)
-        self.assertEqual(len(obj.be_accounts), 1)
-        self.assertEqual(list(obj.be_accounts)[-1], account)
-
-    def testappendBad(self):
-        obj = Budget()
-        transaction = Transaction()
-
-        def testappend():
-            obj.be_accounts.append(transaction)
-
-        self.assertRaises(ValueError, testappend)
-
-    def testsetBad(self):
-        obj = Budget()
-        transaction = Transaction()
-
-        def testset():
-            obj.be_accounts = [transaction]
-
-        self.assertRaises(ValueError, testset)
-
-    def test_str(self):
-        # tests no exceptions when getting the string representation of some entities
-        obj = Transaction()
-        obj.__str__()
-        obj.__unicode__()
-
-        obj2 = Budget()
-        obj2.be_accounts.__str__()
-
-    def jsondefault(self):
-        encoded = json.dumps('test', cls=ComplexEncoder)
-        self.assertEqual(encoded, 'test')
+@pytest.fixture
+def obj(account):
+    budget = Budget()
+    budget.be_accounts = [account]
+    budget.clear_changed_entities()
+    return budget
 
 
-class TestOthers(unittest.TestCase):
-    def test_copy(self):
-        obj = Account()
-        objc = obj.copy()
-        self.assertEqual(obj.id, objc.id)
-        self.assertEqual(obj.get_dict(), objc.get_dict())
+def test_get_ce_addtransactionsubtransaction(client):
+    added_transaction = Transaction()
+    subtransaction1 = Subtransaction(entities_transaction=added_transaction)
+    subtransaction2 = Subtransaction(entities_transaction=added_transaction)
+
+    client.budget.be_transactions.append(added_transaction)
+    client.budget.be_subtransactions.append(subtransaction1)
+    client.budget.be_subtransactions.append(subtransaction2)
+
+    client.session.commit()
+
+    changed_entities = client.budget.get_changed_entities()
+    assert isinstance(changed_entities, dict)
+    assert 1 == len(changed_entities.keys())
+    assert 'be_transaction_groups' == list(changed_entities.keys())[0]
+    transaction_groups = changed_entities['be_transaction_groups']
+
+    assert 1 == len(transaction_groups)
+    assert added_transaction == transaction_groups[0]['be_transaction']
+
+    assert transaction_groups[0]['be_subtransactions'] is not None
+    try:
+        assert {subtransaction1, subtransaction2} == set(transaction_groups[0]['be_subtransactions'])
+    except AttributeError:
+        assert len([subtransaction1, subtransaction2]) == len(set(transaction_groups[0]['be_subtransactions']))
+
+
+def test_arraytype(session):
+    user = User()
+    user.feature_flags = ['featureA', 'feature1']
+    session.add(user)
+    session.commit()
+
+    fetched_user = session.query(User).first()
+    assert user == fetched_user
+
+
+def test_Entityjson(session):
+    obj = MyEntity()
+    session.add(obj)
+    session.commit()
+
+    jsonroundtrip = json.loads(json.dumps(obj, cls=ComplexEncoder))
+    assert {'id': str(obj.id), 'greatfield': 2, 'is_tombstone': False} == jsonroundtrip
+
+
+def test_equality():
+    tr1 = Transaction(id='t')
+    tr2 = Transaction(id='t')
+    assert tr1 == tr2
+
+    tr1 = Transaction()
+    tr2 = Transaction()
+    assert tr1 != tr2
+
+
+def test_repr():
+    tr1 = Transaction(id='t')
+    assert tr1.__repr__() == tr1.__str__()
+
+
+def test_append():
+    obj = Budget()
+    account = Account()
+    obj.be_accounts.append(account)
+    assert len(obj.be_accounts) == 1
+    assert list(obj.be_accounts)[-1] == account
+
+
+def test_str():
+    # tests no exceptions when getting the string representation of some entities
+    obj = Transaction()
+    obj.__str__()
+    obj.__unicode__()
+
+    obj2 = Budget()
+    obj2.be_accounts.__str__()
+
+
+def test_copy():
+    obj = Account()
+    objc = obj.copy()
+    assert obj.id == objc.id
+    assert obj.get_dict() == objc.get_dict()
 
 
 class DummyEntity(Base, Entity):
@@ -217,29 +159,29 @@ class DummyEntity(Base, Entity):
     balance = Column(AmountType)
 
 
-class TestApiDict(unittest.TestCase):
-    def test_input(self):
-        inputdict=dict(
-            id='dummyid/ca85f126-04a1-4196-bbeb-a77acec4b28e',
-            account_type='Checking',
-            date='2016-10-01',
-            balance=1000,
-            is_tombstone=False
-        )
-        dummy = DummyEntity.from_apidict(inputdict)
-        self.assertEqual(dummy.id,inputdict['id'])
-        self.assertEqual(dummy.account_type,AccountTypes.Checking)
-        self.assertEqual(dummy.date, datetime.date(year=2016,month=10,day=1))
-        self.assertEqual(dummy.balance,1)
+def test_input():
+    inputdict = dict(
+        id='dummyid/ca85f126-04a1-4196-bbeb-a77acec4b28e',
+        account_type='Checking',
+        date='2016-10-01',
+        balance=1000,
+        is_tombstone=False
+    )
+    dummy = DummyEntity.from_apidict(inputdict)
+    assert dummy.id == inputdict['id']
+    assert dummy.account_type == AccountTypes.Checking
+    assert dummy.date, datetime.date(year=2016, month=10, day=1)
+    assert dummy.balance == 1
 
-    def test_output(self):
-        inputentity = DummyEntity(
-            account_type = AccountTypes.Checking,
-            date = datetime.date(year=2016,month=10,day=1),
-            balance = 1
-        )
-        entitydict = inputentity.get_apidict()
-        self.assertEqual(entitydict['id'], str(inputentity.id))
-        self.assertEqual(entitydict['account_type'], 'Checking')
-        self.assertEqual(entitydict['date'], '2016-10-01')
-        self.assertEqual(entitydict['balance'], 1000)
+
+def test_output():
+    inputentity = DummyEntity(
+        account_type=AccountTypes.Checking,
+        date=datetime.date(year=2016, month=10, day=1),
+        balance=1
+    )
+    entitydict = inputentity.get_apidict()
+    assert entitydict['id'] == str(inputentity.id)
+    assert entitydict['account_type'] == 'Checking'
+    assert entitydict['date'] == '2016-10-01'
+    assert entitydict['balance'] == 1000
